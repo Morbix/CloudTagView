5fd4f9bfb92bd51df03bebf3683e820e4600385b
Henrique Morbin <morbin_@hotmail.com>
2016-01-27 15:37:59 -0200

    Cleaning comments

--- Example/CloudTagView/ViewController.swift
+++ Example/CloudTagView/ViewController.swift
@@ -17,21 +17,14 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
         cloudView.frame = CGRectMake(0, 20, view.frame.width, 10)
         cloudView.delegate = self
-        //cloudView.backgroundColor = UIColor.blueColor()
         
         view.addSubview(cloudView)
         
         addingNormalTags()
         addingSpecialTags()
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     // MARK: Methods
